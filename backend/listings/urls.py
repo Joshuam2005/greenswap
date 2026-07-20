171a316eab18e74e19824fa2daba9a6fb4e7d844
Joshuam2005 <JoshuaSilva3@my.unt.edu>
2026-07-20 12:27:46 -0500

    Add edit, delete, and sold for listings

--- backend/listings/urls.py
+++ backend/listings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BrowseListingsView, CreateListingView
+from .views import BrowseListingsView, CreateListingView, ListingDetailView
 
-urlpatterns=[
+urlpatterns = [
     path('', BrowseListingsView.as_view(), name='browse-listings'),
     path('create/', CreateListingView.as_view(), name='create-listing'),
+    path('<int:pk>/', ListingDetailView.as_view(), name='listing-detail'),
 ]
