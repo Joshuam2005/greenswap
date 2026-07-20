1529bd35778877b042a88ba4a11a5d24a9c2f42a
Joshuam2005 <JoshuaSilva3@my.unt.edu>
2026-07-20 10:32:06 -0500

    Add create listing

--- backend/listings/urls.py
+++ backend/listings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BrowseListingsView
+from .views import BrowseListingsView, CreateListingView
 
 urlpatterns=[
     path('', BrowseListingsView.as_view(), name='browse-listings'),
+    path('create/', CreateListingView.as_view(), name='create-listing'),
 ]
